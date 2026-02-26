a76b6017870069b284e403adb03f7166fb60a482
iristhepianist <gaurvi.sharma@icloud.com>
2026-02-27 03:36:25 +0530

    Initial 2, Electric Boogaloo

---
+++ src/main/java/quantumstudios/honed/Tags.java
@@ -0,0 +1,7 @@
+package quantumstudios.honed;
+
+public final class Tags {
+    public static final String VERSION = "1.0.0";
+    public static final String MOD_ID = "honed";
+    public static final String MOD_NAME = "Honed";
+}
